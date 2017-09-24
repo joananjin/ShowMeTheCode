a8099dfcf6732518292a58af5e24f17b0e7ce6e7
蒋瑾 <joananjin@163.com>
2017-09-24 17:09:04 +0800

    change the 0000.py

--- 0000.py
+++ 0000.py
@@ -1,7 +1,7 @@
-# -*- coding: UTF-8 -*-
+
 import cv2
 
-img = cv2.imread('pandas.jpg')#hh
+img = cv2.imread('pandas.jpg')
 a = img.shape
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(img, '4', (a[0] - 200, 100), font, 2, (0, 0, 255), 4)
